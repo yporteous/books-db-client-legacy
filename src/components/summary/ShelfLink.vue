8f02a56fc902668268b3f0ce19cb7b422ecec3c3
Younus Porteous <younusporteous@gmail.com>
2018-08-25 20:20:00 +0100

    added interface for deleting shelves

--- src/components/summary/ShelfLink.vue
+++ src/components/summary/ShelfLink.vue
@@ -1,6 +1,19 @@
 <template>
   <div class='shelf-link' @click="goToShelf">
-    {{name}}
+    <div class='shelf-name'>{{name}}</div>
+    <div
+      v-if='name != "All"'
+      class='shelf-menu'
+      @click.stop="showMenu = !showMenu"
+      @mouseleave.stop="showMenu = false"
+    >
+      &hellip;
+      <div class='dropdown-content' :class="{'dropdown-show': showMenu}">
+        <div class='dropdown-link' @click='removeShelf'>
+          <span class='dropdown-text'>Remove shelf</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +21,11 @@
 export default {
   name: 'ShelfLink',
   props: ['name'],
+  data () {
+    return {
+      showMenu: false
+    }
+  },
   computed: {
     path () {
       return this.name.replace(/ /g, '_')
@@ -19,6 +37,9 @@ export default {
         name: 'Bookshelf',
         params: { shelfName: this.path }
       })
+    },
+    removeShelf () {
+      this.$store.dispatch('removeShelf', this.name)
     }
   }
 }
@@ -35,5 +56,32 @@ export default {
 
   padding-left: 10px;
   font-size: 20px;
+}
+.shelf-link>div {
+  display: inline-block;
+}
+.shelf-name {
+  width: 360px;
+}
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f5f5f5;
+  border: 1px solid #444;
+  width: auto;
+  min-width: 150px;
+  max-height: 420px;
+  overflow-y: auto;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+.dropdown-show {
+  display: block;
+}
+.dropdown-link {
+  text-align: center;
+  height: 30px;
+  line-height: 30px;
+  font-size: 15px;
 }
 </style>
